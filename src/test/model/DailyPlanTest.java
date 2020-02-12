08702dbf5cfd78a6a5acab624491770faef3e550
michelleykim <33190160+michelleykim@users.noreply.github.com>
2020-02-11 23:19:07 -0800

    Phase1: Better code coverage. Included test cases deleting menus not in the plan

--- src/test/model/DailyPlanTest.java
+++ src/test/model/DailyPlanTest.java
@@ -60,6 +60,16 @@ public class DailyPlanTest {
     }
 
     @Test
+    void testDeleteMenuNotInPlan() {
+        plan.addMenu("apple", 120, 1.75, true);
+        plan.addMenu("banana", 160, 1.25, true);
+        plan.addMenu("grape", 100, 3.99, true);
+        plan.deleteMenu("pear");
+        assertEquals(plan.getTotalCalories(), 120+160+100);
+        assertEquals(plan.getTotalCost(), 1.75+1.25+3.99);
+    }
+
+    @Test
     void testGenerateMealPlanNoRestriction() {
         plan.generateMealPlan(1200, 10.00, false);
         assertEquals(plan.getTotalCalories(), 1000);
@@ -78,6 +88,13 @@ public class DailyPlanTest {
         plan.generateMealPlan(2200, 50.00, false);
         assertEquals(plan.getTotalCalories(), 2100);
         assertEquals(plan.getTotalCost(), 38.25);
+    }
+
+    @Test
+    void testGenerateMealPlanRestriction2() {
+        plan.generateMealPlan(1200, 50.00, true);
+        assertEquals(plan.getTotalCalories(), 1000);
+        assertEquals(plan.getTotalCost(), 5.25);
     }
 
     @Test
